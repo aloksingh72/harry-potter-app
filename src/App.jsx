bec07e6a42877fc5b8c95ab6f33a6226056d55a6
Alok Singh <aloksingh36757@gmail.com>
2024-07-26 19:28:48 +0530

    completing project of api fetching

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ function App() {
 
   return (
     <>
+    <div className='backgroun'>
+
     <Navbar path="/"/>
     <Routes>
     
@@ -23,6 +25,8 @@ function App() {
      <Route path="/movies" element={<Movies/>} ></Route>
 
     </Routes>
+    </div>
+    
     </>
   )
 }
