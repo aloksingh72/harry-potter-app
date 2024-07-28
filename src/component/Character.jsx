fe872fbb5ea9c49617ee35eb4fe3bb3ebcfaf0f4
Alok Singh <aloksingh36757@gmail.com>
2024-07-28 20:01:42 +0530

    final depolyment of project

--- src/component/Character.jsx
+++ src/component/Character.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import defaultImage from "../assets/pottervector.jpg";
 
 function Character() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
+  // const defaultImage = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hokokyew.com.my%2F%3Fq%3Dharry-potter-wikipedia-bb-88LznmSJ&psig=AOvVaw3NQzfX7DrthDz-DX_zFnct&ust=1722262973894000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCet-T3yYcDFQAAAAAdAAAAABAJ';
   useEffect(() => {
     const getData = async () => {
       try {
@@ -39,8 +40,10 @@ function Character() {
         <h1 className="text-2xl mb-4">Character Data</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {data.map((item, index) => (
-            <div key={index} className="hover:scale-105 transition-all duration-200 bg-white rounded-lg overflow-hidden shadow-lg shadow-orange-500">
-              <img src={item.image} alt="character" className="w-full h-[460px] object-cover" />
+            <div key={index} 
+            className="hover:scale-105 transition-all duration-200 bg-white rounded-lg overflow-hidden shadow-lg shadow-orange-500">
+              <img
+               src={item.image || defaultImage} alt="character" className="w-full h-[460px] object-cover" />
               <div className="p-4">
                 <h2 className="text-lg font-semibold">{item.name}</h2>
               </div>
--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,24 +1,24 @@
 
 import React from "react";
 // component me recieves karenge isopen onclose and book as props 
-function Modal({ isOpen, onClose, book }) {
-  if (!isOpen) return null;
+function Modal(props) {
+  if (!props.isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-stone-500 bg-opacity-65 flex justify-center items-center">
       <div className="bg-white p-4 rounded shadow-lg max-w-sm w-full">
         <div className="flex justify-between items-center">
-          <h2 className="text-xl font-bold">{book.title}</h2>
-          <button onClick={onClose} className="text-red-500">Close</button>
+          <h2 className="text-xl font-bold">{props.book.title}</h2>
+          <button onClick={props.onClose} className="text-red-500">Close</button>
         </div>
         <div>
           {/* <img src={book.cover} alt={book.title} className="w-full h-auto" /> */}
-          <p className="mt-2">Release Date: {book.release_date}</p>
-          <p>No.of Pages: {book.pages} pages</p>
+          <p className="mt-2">Release Date: {props.book.release_date}</p>
+          <p>No.of Pages: {props.book.pages} pages</p>
           
           <p>Description: {
-          book.summary.length>100
-            ?(book.summary.substring(0, 200)) + "...":(book.summary)
+          props.book.summary.length>100
+            ?(props.book.summary.substring(0, 200)) + "...":(props.book.summary)
           }</p>
 
         </div>
